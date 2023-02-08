f33559270db5b5f30f9308eb20e3432565731c17
Imran23A <117780822+Imran23A@users.noreply.github.com>
2023-02-08 15:43:54 +0000

    not local save

--- pubmed_data/ftp_grab.py
+++ pubmed_data/ftp_grab.py
@@ -1,3 +1,4 @@
+import os
 import ftplib
 
 ftp = ftplib.FTP("ftp.ncbi.nlm.nih.gov")
@@ -6,8 +7,15 @@ ftp.cwd("/pubmed/baseline/")
 
 filenames = ftp.nlst()
 
+local_dir = "C:/Users/imran/pubmed_data"
+
+if not os.path.exists(local_dir):
+    os.makedirs(local_dir)
+
+os.chdir(local_dir)
+
 for filename in filenames:
     with open(filename, "wb") as f:
         ftp.retrbinary("RETR " + filename, f.write)
 
-ftp.quit()
+ftp.quit() 
